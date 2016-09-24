7eadf3bfdbaaba4dfc022b8f16782110a79d16a7
Brian Leip <BrianLeip@gmail.com>
2016-09-24 11:23:58 -0700

    Initial Commit

---
+++ 40-MagicPill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  40-MagicPill
+//
+//  Created by Brian Leip on 9/24/16.
+//  Copyright © 2016 Triskelion Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
